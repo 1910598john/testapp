fdb9438554a0de972fa1f143f39415f737b5218b
JM Catamora <1910598john@gmail.com>
2024-06-14 11:28:47 +0800

    commit

--- main.js
+++ main.js
@@ -644,7 +644,7 @@ function viewProfile(id, src) {
                                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z"/>
                                         </svg>
                                     </div>
-                                    <video autoplay style="width:100%;height:100%;object-fit:cover;">
+                                    <video autoplay controls style="width:100%;height:100%;object-fit:cover;">
                                         <source src="${video.src}" type="video/mp4">
                                     </video>
                                 </div>`;
