e3db454675ae7a0984feec9e749d7b9e477ca2ec
JM Catamora <1910598john@gmail.com>
2024-11-25 13:44:36 +0800

    commit

--- main.js
+++ main.js
@@ -2,7 +2,29 @@ var hidden = false;
 var db;
 var currentPerson;
 
+var elem = document.documentElement;
 
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+let fs = false;
+
+if (!fs) {
+    document.documentElement.addEventListener("click", function(event){
+        if (!fs){
+            openFullscreen();
+            fs = true;
+        }
+    })
+}
 
 
 var codes = ['JYP5WmFug1dYTprSKrs6',
