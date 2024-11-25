ccc9cec4f49007b2f204847a7773078eaf341fc7
JM Catamora <1910598john@gmail.com>
2024-11-25 13:40:55 +0800

    commit

--- main.js
+++ main.js
@@ -2,20 +2,6 @@ var hidden = false;
 var db;
 var currentPerson;
 
-var elem = document.documentElement;
-
-/* View in fullscreen */
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
-  }
-}
-
-openFullscreen();
 
 
 
